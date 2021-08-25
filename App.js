932aee2e053d15ee7a64fa78c38984eb449ae912
jianyuanw <69199572+jianyuanw@users.noreply.github.com>
2021-08-25 14:55:37 +0800

    Simple React Native app with serverless user authentication

--- App.js
+++ App.js
@@ -1,12 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import ebconfig from './ebconfig';
+import { EasybaseProvider, useEasybase } from 'easybase-react';
 
 export default function App() {
   return (
+    <EasybaseProvider ebconfig={ebconfig}>
+      <Router />
+    </EasybaseProvider>
+  );
+}
+
+function Router() {
+  const { isUserSignedIn, signOut } = useEasybase();
+
+  return (
+    isUserSignedIn() ? (
+      <View style={styles.container}>
+        <Text>Congrats! You are signed in.</Text>
+        <Button title="Sign Out" onPress={signOut} />
+      </View>
+    ) : (
+      <Account />
+    )
+  );
+}
+
+function Account() {
+  const [userVal, setUserVal] = useState('');
+  const [passVal, setPassVal] = useState('');
+
+  const { signIn, signUp } = useEasybase();
+
+  const clearInputs = () => {
+    setUserVal('');
+    setPassVal('');
+  };
+
+  const handleSignInPress = async () => {
+    console.log('signing in');
+    await signIn(userVal, passVal);
+    clearInputs();
+  };
+
+  const handleSignUpPress = async () => {
+    console.log('signing up');
+    const res = await signUp(userVal, passVal, {
+      created_at: new Date().toString()
+    });
+    console.log(`res.success: ${res.success}`);
+    if (res.success) {
+      await signIn(userVal, passVal);
+    }
+    clearInputs();
+  };
+
+  return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Welcome to React Native Hello World!</Text>
+      <TextInput
+        value={userVal}
+        onChangeText={e => setUserVal(e)}
+        style={styles.accountInput}
+        placeholder="Username"
+      />
+      <TextInput
+        value={passVal}
+        onChangeText={e => setPassVal(e)}
+        style={styles.accountInput}
+        placeholder="Password"
+      />
+      <View style={styles.buttonsWrapper}>
+        <Button title="Sign In" onPress={handleSignInPress} />
+        <Button title="Sign Up" onPress={handleSignUpPress} />
+      </View>
     </View>
   );
 }
@@ -15,7 +82,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  accountInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: '75%',
+    margin: 10,
+    fontSize: 22,
+    textAlign: 'center',
+    alignSelf: 'center',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: '500',
+    fontStyle: 'italic',
+    marginBottom: 30,
+    textAlign: 'center',
+  },
+  buttonsWrapper: {
+    flexDirection: 'row',
+    marginTop: 30,
+    justifyContent: 'space-evenly',
   },
 });
